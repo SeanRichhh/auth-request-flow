2b10da869bd491d7c8aa2fc3aa723e496e0bf5a8
SeanRichhh <sean-richards@outlook.com>
2022-06-14 15:49:24 +0100

    core complete

--- src/router.js
+++ src/router.js
@@ -1,36 +1,39 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-    const username = mockUser.username
-    
-    const token = jwt.sign({username} , 'averysecretsecret')
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-    res.json({ token })
+  if (username !== mockUser.username || password !== mockUser.password) {
+    return res.status(401).json({ error: "Invalid username or passowrd" });
+  }
+
+  const token = jwt.sign({ username }, "averysecretsecret");
+
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  const [bearer, token] = req.headers.authorization.split(" ")
+router.get("/profile", (req, res) => {
+  const [bearer, token] = req.headers.authorization.split(" ");
 
-    try{
-        const payload = jwt.verify(token, "averysecretsecret")
-}catch(e){
-
-}
-res.json({ token })
+  try {
+    const payload = jwt.verify(token, "averysecretsecret");
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+  res.json({ data: mockUser.profile });
 });
-
 
 module.exports = router;
